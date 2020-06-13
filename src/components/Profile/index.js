abf2e530efefe61f8fc2d2a64bd110f39a8a3d45
flipggs <flipggs@gmail.com>
2020-06-13 14:48:43 -0300

    add layout

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Felipe Siqueira</h1>
+    <h2>Programador front-end</h2>
+    <p>
+      Programador front-end e back-end
+    </p>
+  </div>
+);
+
+export default Profile;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const AboutePage = () => (
-  <h1>About page</h1>
-);
+  <Layout>
+    <SEO title="About" />
+    <h1>About page</h1>
+  </Layout>
+)
 
-export default AboutePage;
+export default AboutePage
