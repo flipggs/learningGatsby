75c1a7d1ddf407f634925b4d69a18322b0284878
flipggs <flipggs@gmail.com>
2020-06-13 16:08:59 -0300

    add global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <h1>Felipe</h1>
   </Layout>
 )
 
