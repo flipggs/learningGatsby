8dbb84711392d7e43d5728ea27b0468bc83330a7
flipggs <flipggs@gmail.com>
2020-06-13 15:50:13 -0300

    add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,12 @@ module.exports = {
   siteMetadata: {
     title: `My blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@flipggs`,
+    author: `@flipggs `,
     position: `Front-end developer`
     },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
